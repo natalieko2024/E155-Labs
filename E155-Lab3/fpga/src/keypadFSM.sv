dc1d1fa134bb1eed3c6e02926bbb9e830274dba7
natalieko2024 <ko12nat@gmail.com>
2026-09-19 19:42:38 -0700

    New FSM logic

--- E155-Lab3/fpga/src/keypadFSM.sv
+++ E155-Lab3/fpga/src/keypadFSM.sv
@@ -2,11 +2,11 @@ module keypadFSM(input logic clk, reset,
                 input logic [3:0] cols,
                 input logic [15:0] initialMap, finalMap,
                 input logic [3:0] checkCol,
-                input logic [20:0] count,
-                output logic [2:0] state,
-                output logic scan1EN, scan2EN, displayEN, countReset);
+                input logic [2:0] count1, 
+                input logic [20:0] count2,
+                output logic scan1EN, scan2EN, displayEN, count1Reset, count2Reset);
 
-    logic [1:0] nextState;
+    logic [2:0] state, nextState;
 
     always_ff @(posedge clk, negedge reset) begin
         if (~reset) state <= 3'b000;
@@ -16,55 +16,88 @@ module keypadFSM(input logic clk, reset,
     always_comb begin
 
         case(state)
+            // RESET
             3'b000: begin
                 scan1EN = 1'b0;
                 scan2EN = 1'b0;
                 displayEN = 1'b0;
-                countReset = 1'b1;
+                count1Reset = 1'b0;
+                count2Reset = 1'b0;
                 nextState = 3'b001;
             end
 
+            // SCAN1
             3'b001: begin
                 scan1EN = 1'b1;
                 scan2EN = 1'b0;
                 displayEN = 1'b0;
-                countReset = 1'b0;
-                if (count >= 1199999) nextState = 3'b001;
-                else nextState = 3'b000;
+                count1Reset = 1'b1;
+                count2Reset = 1'b0;
+                if (count1 >= 3) nextState = 3'b010;
+                else nextState = 3'b001;
             end
 
+            // OFF1
             3'b010: begin
+                scan1EN = 1'b0;
+                scan2EN = 1'b0;
+                displayEN = 1'b0;
+                count1Reset = 1'b0;
+                count2Reset = 1'b1;
+                if (count2 >= 1199999) nextState = 3'b011;
+                else nextState = 3'b010;
+            end
+            
+            // SCAN2
+            3'b011: begin
                 scan1EN = 1'b0;
                 scan2EN = 1'b1;
                 displayEN = 1'b0;
-                countReset = 1'b0;
-                if (~(initialMap == finalMap) || ~$onehot(finalMap)) nextState = 3'b000;
-                else if ((initialMap == finalMap) && $onehot(finalMap)) nextState = 3'b011;
-                else nextState = 3'b010;
+                count1Reset = 1'b1;
+                count2Reset = 1'b0;
+                if (count1 >= 3) nextState = 3'b100;
+                else nextState = 3'b011;
             end
 
-            3'b011: begin
-                scan1EN = 1'b0;
-                scan2EN = 1'b0;
-                displayEN = 1'b1;
-                countReset = 1'b0;
-                nextState = 3'b100;
-            end
-
+            // OFF 2
             3'b100: begin
                 scan1EN = 1'b0;
                 scan2EN = 1'b0;
                 displayEN = 1'b0;
-                countReset = 1'b0;
-                if (~(checkCol && cols)) nextState = 3'b000;
+                count1Reset = 1'b0;
+                count2Reset = 1'b0;
+                if (~(initialMap == finalMap) || ~$onehot(finalMap)) nextState = 3'b000;
+                else if ((initialMap == finalMap) && $onehot(finalMap)) nextState = 3'b101;
                 else nextState = 3'b100;
+            end
+
+            // DISPLAY
+            3'b101: begin
+                scan1EN = 1'b0;
+                scan2EN = 1'b0;
+                displayEN = 1'b1;
+                count1Reset = 1'b0;
+                count2Reset = 1'b0;
+                nextState = 3'b110;
+            end
+
+            // HOLD
+            3'b110: begin
+                scan1EN = 1'b0;
+                scan2EN = 1'b0;
+                displayEN = 1'b0;
+                count1Reset = 1'b0;
+                count2Reset = 1'b0;
+                if (~(checkCol && cols)) nextState = 3'b000;
+                else nextState = 3'b110;
             end
 
             default: begin
                 scan1EN = 1'b0;
                 scan2EN = 1'b0;
                 displayEN = 1'b0;
-                countReset = 1'b1;
+                count1Reset = 1'b1;
+                count2Reset = 1'b0;
                 nextState = 3'b000;
             end
         endcase
--- E155-Lab3/fpga/src/lab3_nk.sv
+++ E155-Lab3/fpga/src/lab3_nk.sv
@@ -4,36 +4,36 @@ module lab3_nk(input logic reset,
                 output logic [6:0] segWrite,
                 output logic anodeLeft, anodeRight);
 
-	logic clk, stepDownClk, clkDontUse, countReset, scan1EN, scan2EN, displayEN;
+	logic clk, scanClk, debounceClk, countReset, scan1EN, scan2EN, displayEN;
 	logic [16:0] countDontUse;
-	logic [20:0] count;
+	logic [20:0] debounceCount;
 	logic [3:0] switches, syncCols, checkCol, switchRight, switchLeft, switchRightDontUse, switchLeftDontUse, switchWrite;
 	logic [15:0] initialMap, finalMap;
-	logic [2:0] state;
+	logic [2:0] scanCount;
 
     // Set up the internal HSOSC to provide a 24MHz oscillation frequency output
 	HSOSC #(.CLKHF_DIV("0b01")) 
 		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
 		
-	freqconverter #(.MAX(80000), .WIDTH(17)) clock(clk, reset, 1'b1, stepDownClk, countDontUse);
-    freqconverter #(.MAX(1200000), .WIDTH(21)) counter(clk, countReset, 1'b1, clkDontUse, count);	
+	freqconverter #(.MAX(4), .WIDTH(3)) clock(clk, countReset, 1'b1, scanClk, scanCount);
+    freqconverter #(.MAX(1200000), .WIDTH(21)) counter(clk, displayCountReset, 1'b1, debounceClk, debounceCount);	
 
     // scanning module to send the rotating scanning signal to keypad rows
-	scan s1(stepDownClk, reset, (scan1EN || scan2EN), rows);
+	scan s1(clk, reset, (scan1EN || scan2EN), rows);
 
-    synchronizer #(.WIDTH(4)) sync(stepDownClk, cols, syncCols);
+    synchronizer #(.WIDTH(4)) sync(clk, cols, syncCols);
 
-    keyMapping  initialMapping(stepDownClk, reset, scan1EN, rows, syncCols, initialMap);
-    keyMapping  finalMapping(stepDownClk, reset, scan2EN, rows, syncCols, finalMap);
+    keyMapping  initialMapping(clk, reset, scan1EN, rows, syncCols, initialMap);
+    keyMapping  finalMapping(clk, reset, scan2EN, rows, syncCols, finalMap);
 
     checkUnpressed      cu1(finalMap, checkCol);
 
-    keypadFSM(stepDownClk, reset, syncCols, initialMap, finalMap, checkCol, count, state, scan1EN, scan2EN, displayEN, countReset);
+    keypadFSM(clk, reset, syncCols, initialMap, finalMap, checkCol, scanCount, debounceCount, scan1EN, scan2EN, displayEN, countReset, displayCountReset);
 
     keySwitchConverter      mapSwitches(finalMap, switches);
 
-    flipFlop #(.WIDTH(4)) findSegRight(stepDownClk, switches, switchRight);
-    flipFlop #(.WIDTH(4)) findSegLeft(stepDownClk, switchRight, switchLeft);
+    flipFlop #(.WIDTH(4)) findSegRight(clk, displayEN, switches, switchRight);
+    flipFlop #(.WIDTH(4)) findSegLeft(clk, displayEN, switchRight, switchLeft);
 
     // multiplexing counter module to write outputs at a frequency
 	muxCounter mc1(clk, reset, 1'b1, 4'b0000, anodeLeft, anodeRight, switchLeftDontUse, switchRightDontUse);
