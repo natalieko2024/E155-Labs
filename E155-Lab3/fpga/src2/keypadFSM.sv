cc3ed00d281b3106346d905bc41731e15428394f
nko <86842497+natalieko2024@users.noreply.github.com>
2026-09-20 17:07:18 -0700

    Revert "work"
    This reverts commit e51670ade05104bc432334de6fb8cdf86090f3d8.

--- E155-Lab3/fpga/src2/keypadFSM.sv
+++ E155-Lab3/fpga/src2/keypadFSM.sv
@@ -105,8 +105,9 @@ module keypadFSM(input logic clk, reset,
                 scanCountRST = 1'b0;    // Stop counting to scan, reset counter
                 debounceRST = 1'b0;
                 displayEN = 1'b0;
-                if (finalMap[position] != initialMap[position]) nextState = 3'b000;    // If pressed key isn't pressed anymore, move to RESET state
-                else nextState = 3'b111;    // Else stay in OFF3 state
+                //if (finalMap[position] != initialMap[position]) nextState = 3'b000;    // If pressed key isn't pressed anymore, move to RESET state
+                //else nextState = 3'b111;    // Else stay in OFF3 state
+				nextState = 3'b000;
             end
 
             default: begin
