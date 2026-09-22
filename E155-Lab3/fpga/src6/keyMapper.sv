45c60e5a751f14994186a634346e191bd652bb0d
natalieko2024 <ko12nat@gmail.com>
2026-09-22 09:51:57 -0700

    possible fix

--- E155-Lab3/fpga/src6/keyMapper.sv
+++ E155-Lab3/fpga/src6/keyMapper.sv
@@ -3,6 +3,7 @@
 
 module keyMapper(input logic clk, reset, enable,
                 input logic [3:0] rows, cols,
+                input logic [8:0] row0Count, row1Count, row2Count, row3Count,
                 output logic [15:0] map);
 
     logic [15:0] newMap, tempMap;
@@ -18,10 +19,10 @@ module keyMapper(input logic clk, reset, enable,
     // (cols are active low, so ~cols will write 1 when the key is pressed)
 	
 	always_comb begin
-		if (rows[0]) newMap = {tempMap[15:4], ~cols};
-		else if (rows[1]) newMap = {tempMap[15:8], ~cols, tempMap[3:0]};
-		else if (rows[2]) newMap = {tempMap[15:12], ~cols, tempMap[7:0]};
-		else if (rows[3]) newMap = {~cols, tempMap[11:0]};
+		if (rows[0] && (row0Count == 200)) newMap = {tempMap[15:4], ~cols};
+		else if (rows[1] && (row1Count == 200)) newMap = {tempMap[15:8], ~cols, tempMap[3:0]};
+		else if (rows[2] && (row2Count == 200)) newMap = {tempMap[15:12], ~cols, tempMap[7:0]};
+		else if (rows[3] && (row3Count == 200)) newMap = {~cols, tempMap[11:0]};
 		else newMap = 16'b0;
 	end
 
--- E155-Lab3/fpga/src6/lab3_nk.sv
+++ E155-Lab3/fpga/src6/lab3_nk.sv
@@ -4,11 +4,12 @@ module lab3_nk(input logic reset,
                 output logic [1:0] anodes,
                 output logic [6:0] segWrite);
 
-    logic clk, displayEN, anodeClk;
+    logic clk, displayEN, anodeClk, row0Clk, row1Clk, row2Clk, row3Clk;
     logic [3:0] syncCols, switches;
     logic [15:0] keyMap, debouncedKeyMap;
     logic [6:0] segs, segRight, segLeft;
     logic [32:0] anodeCount;
+    logic [8:0] row0Count, row1Count, row2Count, row3Count;
     
     // Set up the internal HSOSC to provide a 24MHz oscillation frequency output
 	HSOSC #(.CLKHF_DIV("0b01")) 
@@ -19,9 +20,13 @@ module lab3_nk(input logic reset,
     scan #(.COUNTWIDTH(9), .COUNTMAX(500)) scanner(clk, reset, 1'b1, rows);
 
     freqconverter #(.WIDTH(32), .MAX(200001)) anodeSwitcher(clk, reset, 1'b1, anodeClk, anodeCount);
+    freqconverter #(.WIDTH(9), .MAX(500)) trackRow1(clk, (rows[0]), 1'b1, row1Clk, row1Count);
+    freqconverter #(.WIDTH(9), .MAX(500)) trackRow2(clk, (rows[1]), 1'b1, row2Clk, row2Count);
+    freqconverter #(.WIDTH(9), .MAX(500)) trackRow3(clk, (rows[2]), 1'b1, row3Clk, row3Count);
+    freqconverter #(.WIDTH(9), .MAX(500)) trackRow0(clk, (rows[3]), 1'b1, row0Clk, row0Count);
 
     //keyMap  getMap(clk, reset, rows, syncCols, keyMap);
-    keyMapper getMap(clk, reset, 1'b1, rows, syncCols, keyMap);
+    keyMapper getMap(clk, reset, 1'b1, rows, syncCols, row0Count, row1Count, row2Count, row3Count, keyMap);
 
     debouncer debounce(clk, reset, keyMap, debouncedKeyMap);
 
